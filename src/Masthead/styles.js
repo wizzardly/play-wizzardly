e797eb33798d7663efbb1fc82d774d3747f4e7b1
Eric Garside <garside@gmail.com>
2018-05-13 21:17:11 -0400

    Lint cleanup

--- src/Masthead/styles.js
+++ src/Masthead/styles.js
@@ -1,13 +1,9 @@
-export default theme => ({
+export default () => ({
   root: {
     flexGrow: 1,
   },
   flex: {
     flex: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
   },
 })
 
