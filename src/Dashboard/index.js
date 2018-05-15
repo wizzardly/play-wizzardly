ee628846a3de91c04c038e337097ab894c222f8b
Eric Garside <garside@gmail.com>
2018-05-15 02:52:05 -0400

    Removing duplication for testing

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { styledComponentPropType } from 'shared/shapes'
 
 import { withStyles } from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
-import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid'
+// import Typography from '@material-ui/core/Typography'
+// import Paper from '@material-ui/core/Paper'
+// import Grid from '@material-ui/core/Grid'
 
 import styles from './styles'
 
@@ -12,17 +12,7 @@ export function Dashboard(props) {
   const { classes } = props
 
   return <div id="dashboard">
-    <Grid container justify="center" className={classes.root}>
-      <Grid item xs={6}>
-        <Paper className={classes.paper}>
-          <Typography variant="display3" gutterBottom>
-            Welcome
-          </Typography>
-
-          You Are Here
-        </Paper>
-      </Grid>
-    </Grid>
+    Welcome!
   </div>
 }
 
--- src/Dashboard/index.spec.js
+++ src/Dashboard/index.spec.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { withMounted, withWrapper } from 'shared/specs'
 
-import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
-
 import { Dashboard } from './index.js'
 
 describe('Dashboard', () => {
@@ -11,16 +8,8 @@ describe('Dashboard', () => {
   const subject = () => <Dashboard classes={{ root: '', paper: '' }} />
 
   withMounted(subject, identifier, (mounted, example) => {
-    example.contains('Welcome')
+    example.contains('Welcome!')
   })
 
-  withWrapper(subject, identifier, wrapper => {
-    it('contains a Paper', () => {
-      expect(wrapper.find(Paper).length).toBe(1)
-    })
-
-    it('contains a Grid', () => {
-      expect(wrapper.find(Grid).length).toBeGreaterThan(0)
-    })
-  })
+  withWrapper(subject, identifier)
 })
