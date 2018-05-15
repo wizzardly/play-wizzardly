00c2596ecc3a7a9da8f4f98139ae62d90a733ffe
Eric Garside <garside@gmail.com>
2018-05-14 20:37:33 -0400

    DRY'd spec

--- src/shared/specs.js
+++ src/shared/specs.js
@@ -5,25 +5,21 @@ import { mount, shallow } from 'enzyme'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 
+const rendersOne = (source, selector) => expect(source.find(selector).length).toBe(1)
+
 export function withMounted(subject, identifier, fn) {
   describe('with mounted', () => {
     const mounted = mount(subject())
 
-    it('has the correct identifier', () => {
-      expect(mounted.find(identifier).length).toBe(1)
-    })
+    it('has the correct identifier', () => rendersOne(mounted, identifier))
 
     if (fn) {
       const contains = text => {
-        it('contains the expected text', () => {
-          expect(mounted.html()).toContain(text)
-        })
+        it('contains the expected text', () => expect(mounted.html()).toContain(text))
       }
 
       const rendersOne = selector => {
-        it('renders the expected selector', () => {
-          expect(mounted.find(selector).length).toBe(1)
-        })
+        it('renders the expected selector', () => rendersOne(mounted, selector))
       }
 
       fn(mounted, { contains, rendersOne })
@@ -35,12 +31,14 @@ export function withWrapper(subject, identifier, fn) {
   describe('with wrapper', () => {
     const wrapper = shallow(subject())
 
-    it('has the correct identifier', () => {
-      expect(wrapper.is(identifier)).toBe(true)
-    })
+    it('has the correct identifier', () => expect(wrapper.is(identifier)).toBe(true))
+
+    const rendersOne = selector => {
+      it('renders the expected selector', () => rendersOne(wrapper, selector))
+    }
 
     if (fn) {
-      fn(wrapper)
+      fn(wrapper, { rendersOne })
     }
   })
 }
