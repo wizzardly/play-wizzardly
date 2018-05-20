f7b95e91754fcc42f4fb09c2c796ab6f68e860eb
Eric Garside <garside@gmail.com>
2018-05-20 00:03:05 -0400

    Test cleanup

--- src/Login/LoginForm/index.spec.js
+++ src/Login/LoginForm/index.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 
 // import Grid from '@material-ui/core/Grid'
 // import Paper from '@material-ui/core/Paper'
@@ -9,6 +9,16 @@ import { LoginForm } from './index.js'
 describe('LoginForm', () => {
   const subject = () => <LoginForm classes={{ textField: '', button: '' }} />
 
+  describe('when mounted', () => {
+    const mounted = mount(subject())
+
+    describe('onSubmit', () => {
+      beforeEach(() => mounted.find('button#login-form-submit').simulate('click'))
+
+      it('submits the form when the submit button is clicked', () => expect(true).toBe(true))
+    })
+  })
+
   describe('when shallow rendered', () => {
     const wrapper = shallow(subject())
 
@@ -16,11 +26,5 @@ describe('LoginForm', () => {
     // it('renders Paper', () => expect(wrapper.find(Paper)).toHaveLength(1))
     // it('renders Grid', () => expect(wrapper.find(Grid)).not.toHaveLength(0))
     // it('renders LoginForm', () => expect(wrapper.find(LoginFormf)).toHaveLength(1))
-
-    describe('onSubmit', () => {
-      beforeEach(() => wrapper.find('#login-form-submit').simulate('click'))
-
-      it('submits the form', () => expect(true).toBe(true))
-    })
   })
 })
