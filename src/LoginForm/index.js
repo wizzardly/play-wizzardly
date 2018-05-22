59a4f1ca15e86fef79278f7c9f8f13a839b6da26
Eric Garside <garside@gmail.com>
2018-05-22 00:29:58 -0400

    Connecting loginform

--- src/LoginForm/index.js
+++ src/LoginForm/index.js
@@ -1,7 +1,8 @@
+import { connect } from 'react-redux'
 import { withStyles } from '@material-ui/core/styles'
 
 import styles from './styles'
 
 import LoginForm from './LoginForm'
 
-export default withStyles(styles)(LoginForm)
+export default connect()(withStyles(styles)(LoginForm))
