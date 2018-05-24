196640f412f26c8a4a0fb9326f621cd1070c004c
Eric Garside <garside@gmail.com>
2018-05-24 03:12:43 -0400

    Cleanup

--- src/Site/Site.spec.js
+++ src/Site/Site.spec.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import { authenticationInitialState } from 'data/initialState'
 
 import SignIn from 'SignIn'
 import Header from 'Header'
@@ -8,16 +9,16 @@ import Layout from 'Layout'
 import Site from './Site'
 
 describe('Site', () => {
-  const subject = props => <Site {...props} />
+  const subject = (authentication = {}, classes = {}) => <Site authentication={authentication} classes={classes} />
 
   describe('when unauthenticated', () => {
-    const wrapper = shallow(subject({ authentication: { signedIn: false } }))
+    const wrapper = shallow(subject({ ...authenticationInitialState, signedIn: false }))
 
     it('returns SignIn', () => expect(wrapper.is(SignIn)).toBe(true))
   })
 
   describe('when authenticated', () => {
-    const wrapper = shallow(subject({ authentication: { signedIn: true } }))
+    const wrapper = shallow(subject({ ...authenticationInitialState, signedIn: true }))
 
     it('has the expected selector', () => expect(wrapper.is('#site')).toBe(true))
     it('renders Header', () => expect(wrapper.find(Header)).toExist())
