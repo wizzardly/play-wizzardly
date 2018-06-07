caec704ec4897f8398cfb2869da08973c037c61e
Eric Garside <garside@gmail.com>
2018-06-07 00:22:42 -0400

    Cleaning up index

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,10 +1,7 @@
 import { connect } from 'react-redux'
-import { withStyles } from '@material-ui/core/styles'
-
-import styles from '../UIHeader/styles'
 
 import Header from './Header'
 
 const mapStateToProps = state => ({ site: state.site })
 
-export default withStyles(styles)(connect(mapStateToProps)(Header))
+export default connect(mapStateToProps)(Header)
