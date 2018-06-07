77ad6b5b9146852662c83d530948c09edaa6d8cc
Eric Garside <garside@gmail.com>
2018-06-07 00:53:02 -0400

    UIMainMenu

--- src/MainMenu/index.js
+++ src/MainMenu/index.js
@@ -1,10 +1,7 @@
 import { connect } from 'react-redux'
-import { withStyles } from '@material-ui/core/styles'
-
-import styles from './styles'
 
 import MainMenu from './MainMenu'
 
 const mapStateToProps = state => ({ site: state.site })
 
-export default withStyles(styles)(connect(mapStateToProps)(MainMenu))
+export default connect(mapStateToProps)(MainMenu)
