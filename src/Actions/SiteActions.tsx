68a6f5e034c96d5fcf1aa179a34d0cd1a932e05b
Eric Garside <garside@gmail.com>
2018-06-10 13:24:23 -0400

    Actions: TypeScript

---
+++ src/Actions/SiteActions.tsx
@@ -0,0 +1,12 @@
+import { IAction } from './types'
+
+export const SIGN_OUT_DIALOG_SHOW = Symbol('SIGN_OUT_DIALOG_SHOW')
+export const SIGN_OUT_DIALOG_HIDE = Symbol('SIGN_OUT_DIALOG_HIDE')
+export const MAIN_MENU_SHOW = Symbol('MAIN_MENU_SHOW')
+export const MAIN_MENU_HIDE = Symbol('MAIN_MENU_HIDE')
+
+export const ShowSignOutDialog = (): IAction => ({ type: SIGN_OUT_DIALOG_SHOW })
+export const HideSignOutDialog = (): IAction => ({ type: SIGN_OUT_DIALOG_HIDE })
+
+export const ShowMainMenu = (): IAction => ({ type: MAIN_MENU_SHOW })
+export const HideMainMenu = (): IAction => ({ type: MAIN_MENU_HIDE })
