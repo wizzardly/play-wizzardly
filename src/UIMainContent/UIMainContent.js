ebd6ce7ed23dcc249c5cb3f6f38e939ba0bc8b51
Eric Garside <garside@gmail.com>
2019-01-04 20:48:06 -0500

    Cleanup

--- src/UIMainContent/UIMainContent.js
+++ src/UIMainContent/UIMainContent.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import { styledComponentProp } from 'props'
 
-// import Game from 'Game'
-
 function UIMainContent({ classes }) {
   return <section id="main-content" className={classes.content}>
     <div className={classes.drawerHeader} />
-    {/*<Game />*/}
   </section>
 }
 
